62c0e0a1ca5ac9f8e6363db2d3953e945786ce00
Leonardo Pena <leonardo.pena@traveltradegroup.com>
2023-11-19 13:17:38 -0300

    refactoring Task request and model to reuse code

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
 use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -40,37 +41,27 @@ Route::get('/tasks/{task}', function (Task $task) {
     ]);
 })->name('tasks.show');
 
-Route::post('/tasks', function (Request $request){
-    $data = $request->validate([
-        'title' =>  'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required'
-    ]);
-
-    $task = new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-
-    $task->save();
+Route::post('/tasks', function (TaskRequest $request){
+//    $data = $request->validated();
+//    $task = new Task;
+//    $task->title = $data['title'];
+//    $task->description = $data['description'];
+//    $task->long_description = $data['long_description'];
+//    $task->save();
+    $task = Task::created($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task created successfully!');
 
 })->name('tasks.store');
 
-Route::put('/tasks/{task}', function (Task $task, Request $request){
-    $data = $request->validate([
-        'title' =>  'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required'
-    ]);
-
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-
-    $task->save();
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request){
+//    $data = $request->validated();
+//    $task->title = $data['title'];
+//    $task->description = $data['description'];
+//    $task->long_description = $data['long_description'];
+//    $task->save();
+    $task->update($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task updated successfully!');
