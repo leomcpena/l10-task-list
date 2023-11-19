5f9e7abbad671b33162ce8e9637d7a2d5b3e337b
Leonardo Pena <leonardo.pena@traveltradegroup.com>
2023-11-19 12:53:27 -0300

    add edit task

--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,25 @@ Route::post('/tasks', function (Request $request){
 
 })->name('tasks.store');
 
+Route::put('/tasks/{id}', function ($id, Request $request){
+    $data = $request->validate([
+        'title' =>  'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = Task::findOrFail($id);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task updated successfully!');
+
+})->name('tasks.update');
+
 Route::fallback(function () {
     return 'Still got somewhere!';
 });
