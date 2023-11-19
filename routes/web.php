beaf1a5209ea804645e07836e99739098266474b
Leonardo Pena <leonardo.pena@traveltradegroup.com>
2023-11-19 08:55:42 -0300

    create task form

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,90 +15,49 @@ use Symfony\Component\HttpFoundation\Response;
 |
 */
 
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
-
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
-
 Route::get('/', function (){
     return redirect()->route('tasks.index');
 });
 
-Route::get('tasks', function () use ($tasks) {
+Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => $tasks
+        'tasks' => Task::latest()->get()
     ]);
 })->name('tasks.index');
 
-Route::get('tasks/{id}', function ($id) use ($tasks) {
-    $task = collect($tasks)->firstWhere('id', $id);
+Route::view('/tasks/create', 'create')
+    ->name('tasks.create');
 
-    if (!$task) {
-        abort(Response::HTTP_NOT_FOUND);
-    }
+Route::get('/tasks/{id}/edit', function ($id) {
+    return view('edit', [
+        'task' => Task::findOrFail($id)
+    ]);
+})->name('tasks.edit');
 
-    return view('show', ['task' => $task]);
+Route::get('/tasks/{id}', function ($id) {
+    return view('show', [
+        'task' => Task::findOrFail($id)
+    ]);
 })->name('tasks.show');
 
-// Route::get('/xxx', function () {
-//     return 'Hello';
-// })->name('hello');
+Route::post('/tasks', function (Request $request){
+    $data = $request->validate([
+        'title' =>  'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
 
-// Route::get('/hallo', function () {
-//     return redirect()->route('hello');
-// });
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
 
-// Route::get('/greet/{name}', function ($name) {
-//     return 'Hello ' . $name . '!';
-// });
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task created successfully!');
+
+})->name('tasks.store');
 
 Route::fallback(function () {
     return 'Still got somewhere!';
