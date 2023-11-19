4dc075168a58547c9ff312f3a0d1e8c4b6e32f00
Leonardo Pena <leonardo.pena@traveltradegroup.com>
2023-11-19 13:31:49 -0300

    add: delete tasks

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Requests\TaskRequest;
 use App\Models\Task;
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpFoundation\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -42,12 +41,6 @@ Route::get('/tasks/{task}', function (Task $task) {
 })->name('tasks.show');
 
 Route::post('/tasks', function (TaskRequest $request){
-//    $data = $request->validated();
-//    $task = new Task;
-//    $task->title = $data['title'];
-//    $task->description = $data['description'];
-//    $task->long_description = $data['long_description'];
-//    $task->save();
     $task = Task::create($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])
@@ -56,17 +49,19 @@ Route::post('/tasks', function (TaskRequest $request){
 })->name('tasks.store');
 
 Route::put('/tasks/{task}', function (Task $task, TaskRequest $request){
-//    $data = $request->validated();
-//    $task->title = $data['title'];
-//    $task->description = $data['description'];
-//    $task->long_description = $data['long_description'];
-//    $task->save();
     $task->update($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task updated successfully!');
 
 })->name('tasks.update');
+
+Route::delete('/tasks/{task}', function (Task $task){
+    $task->delete();
+
+    return redirect()->route('tasks.index')
+        ->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
 
 Route::fallback(function () {
     return 'Still got somewhere!';
