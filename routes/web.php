97e1f6f042272e1ba621bf0a7ea453f0b1781d03
Leonardo Pena <leonardo.pena@traveltradegroup.com>
2023-11-19 13:05:43 -0300

    add: route model binding

--- routes/web.php
+++ routes/web.php
@@ -28,15 +28,15 @@ Route::get('/tasks', function () {
 Route::view('/tasks/create', 'create')
     ->name('tasks.create');
 
-Route::get('/tasks/{id}/edit', function ($id) {
+Route::get('/tasks/{task}/edit', function (Task $task) {
     return view('edit', [
-        'task' => Task::findOrFail($id)
+        'task' => $task
     ]);
 })->name('tasks.edit');
 
-Route::get('/tasks/{id}', function ($id) {
+Route::get('/tasks/{task}', function (Task $task) {
     return view('show', [
-        'task' => Task::findOrFail($id)
+        'task' => $task
     ]);
 })->name('tasks.show');
 
@@ -54,26 +54,25 @@ Route::post('/tasks', function (Request $request){
 
     $task->save();
 
-    return redirect()->route('tasks.show', ['id' => $task->id])
+    return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task created successfully!');
 
 })->name('tasks.store');
 
-Route::put('/tasks/{id}', function ($id, Request $request){
+Route::put('/tasks/{task}', function (Task $task, Request $request){
     $data = $request->validate([
         'title' =>  'required|max:255',
         'description' => 'required',
         'long_description' => 'required'
     ]);
 
-    $task = Task::findOrFail($id);
     $task->title = $data['title'];
     $task->description = $data['description'];
     $task->long_description = $data['long_description'];
 
     $task->save();
 
-    return redirect()->route('tasks.show', ['id' => $task->id])
+    return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task updated successfully!');
 
 })->name('tasks.update');
